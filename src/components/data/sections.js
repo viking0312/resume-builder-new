03584ee2795b23da4869d60300a69785f6b37bd5
maulik.mynt@perennialsys.com <maulik.mynt@perennialsys.com>
2022-06-07 17:18:28 +0530

    add firebase config

---
+++ src/components/data/sections.js
@@ -0,0 +1,11 @@
+const sections = [
+  "Basic information",
+  "Description",
+  "Education",
+  "Experience",
+  "Certifications",
+  "Hobbies",
+  "Languages known",
+];
+
+export default sections;

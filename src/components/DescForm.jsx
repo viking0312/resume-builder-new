0440d16765fef819f396031ac3b3edf6db647e37
viking0312 <m.maulik96@gmail.com>
2022-06-08 21:26:23 +0530

    change forms based on current step

---
+++ src/components/DescForm.jsx
@@ -0,0 +1,18 @@
+import { Grid, TextareaAutosize } from "@mui/material";
+
+const DescForm = () => {
+  return (
+    <>
+      <Grid item>
+        <TextareaAutosize
+          aria-label="minimum height"
+          minRows={3}
+          placeholder="Minimum 3 rows"
+          style={{ width: 200 }}
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default DescForm;
---
+++ src/components/InfoForm.jsx
@@ -0,0 +1,42 @@
+import { Grid, TextField } from "@mui/material";
+
+const InfoForm = () => {
+  return (
+    <>
+      <Grid item>
+        <TextField
+          required
+          id="outlined-required"
+          label="Name"
+          defaultValue=""
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          required
+          id="outlined-required"
+          label="Contact number"
+          defaultValue=""
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          required
+          id="outlined-required"
+          label="Email ID"
+          defaultValue=""
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          required
+          id="outlined-required"
+          label="Address"
+          defaultValue=""
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default InfoForm;

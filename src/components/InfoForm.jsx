7d0a3be792f3c93675f5fb918d062e9c9cfc9659
maulik.mynt@perennialsys.com <maulik.mynt@perennialsys.com>
2022-06-11 15:47:48 +0530

    add state for the form component

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -1,7 +1,9 @@
-import React from "react";
 import { Grid, TextField } from "@mui/material";
+import React from "react";
+import PropTypes from "prop-types";
 
-const InfoForm = () => {
+const InfoForm = (props) => {
+  const { values, setValues } = props;
   return (
     <>
       <Grid item>
@@ -9,6 +11,8 @@ const InfoForm = () => {
           required
           id="outlined-required"
           label="Name"
+          type="text"
+          onChange={(e) => setValues({ ...values, name: e.target.value })}
           defaultValue=""
         />
       </Grid>
@@ -17,27 +21,40 @@ const InfoForm = () => {
           required
           id="outlined-required"
           label="Contact number"
+          type="tel"
+          onChange={(e) =>
+            setValues({ ...values, contantNumber: e.target.value })
+          }
           defaultValue=""
         />
       </Grid>
       <Grid item>
         <TextField
           required
+          type="email"
           id="outlined-required"
           label="Email ID"
+          onChange={(e) => setValues({ ...values, email: e.target.value })}
           defaultValue=""
         />
       </Grid>
       <Grid item>
         <TextField
           required
+          type="text"
           id="outlined-required"
           label="Address"
+          onChange={(e) => setValues({ ...values, address: e.target.value })}
           defaultValue=""
         />
       </Grid>
     </>
   );
+};
+
+InfoForm.propTypes = {
+  values: PropTypes.object,
+  setValues: PropTypes.func,
 };
 
 export default InfoForm;

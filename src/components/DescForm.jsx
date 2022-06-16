d81278d31bfedeea4e5e171eb39a06c834803f30
shikha gupta <shikhaguptasg94@gmail.com>
2022-06-16 17:00:38 +0530

    Description textfield changes

--- src/components/DescForm.jsx
+++ src/components/DescForm.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { Grid, TextareaAutosize } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import PropTypes from "prop-types";
 
 const DescForm = (props) => {
-  const { values, setValues } = props;
+  const { values, setValues, setNextDisabled } = props;
+  if(values.description == "" || values.description.length < 20){
+    setNextDisabled(true);
+  }else{
+    setNextDisabled(false);
+  }
+  const setDescValue = (e) => {
+    console.log(e);
+    var val = e.target.value;
+    setValues({ ...values, description: val });
+  }
   return (
     <>
-      <Grid item>
-        <TextareaAutosize
-          aria-label="minimum height"
-          minRows={3}
-          placeholder="Minimum 3 rows"
-          onChange={(e) =>{ setValues({...values, description: e.target.value})}}
-          style={{ width: 200 }}
+      <Grid item 
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}>
+        <TextField
+          required
+          //error={formError.description.isErrored}
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          onChange={(e) => setDescValue(e)}
+          defaultValue=""
+          style={ {width: "500px"}}
+          //helperText={formError.description.errorMessage}
         />
       </Grid>
     </>
@@ -22,5 +40,6 @@ const DescForm = (props) => {
 DescForm.propTypes = {
   values: PropTypes.object,
   setValues: PropTypes.func,
+  setNextDisabled: PropTypes.func,
 };
 export default DescForm;
